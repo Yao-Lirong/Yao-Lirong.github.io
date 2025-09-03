fc261ae0318b8b5de5ee38639a04549a1b1d28c2
Yao-Lirong <90729853+Yao-Lirong@users.noreply.github.com>
2025-09-03 18:16:17 +0000

    Deploying to gh-pages from @ Yao-Lirong/Yao-Lirong.github.io@f162b09b6b2dbaabed9499103ecca8fb7a8a199d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
+        },{id: "nav-resume",
+          title: "resume",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/resume/";
+          },
         },{id: "post-matryoshka-representation-learning-adaptive-retrieval-and-binary-vector-search",
         
           title: 'Matryoshka Representation Learning, Adaptive Retrieval and Binary Vector Search <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
